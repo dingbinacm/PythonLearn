f90165333922b896b214ba3f23ede3225f8871e7
Bin Ding <bin.ding@shopee.com>
2019-08-20 11:24:03 +0800

    initail

---
+++ venv/com/learn/basic/basic.py
@@ -0,0 +1,24 @@
+print(0x10)
+print("I am ok")
+print(True and False)
+print(True or False)
+print(not True)
+# 特殊的值，空值
+print(None)
+a = 123
+print(a)
+a = True
+print(a)
+a = 'abc'
+print(a)
+b = a
+a = 'ddd'
+print(b)
+# 在Python中，通常用全部大写的变量名表示常量
+PI = 3.141592653
+# / 精确除法， // 地板除法， %
+print(4//3)
+print("hello", "world")
+# '' ""都可以表示字符串
+print("hello", "\"workld\"")
+
